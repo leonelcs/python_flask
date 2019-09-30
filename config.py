6ab583a20fd09fab1c0632e98803096321faa4cf
Leonel Candido da Silva <leonel.da.silva@capgemini.com>
2019-09-30 15:06:51 +0200

    fixing chapter 3

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 class Config(object): 
-    pass 
+    POSTS_PER_PAGE = 10
  
 class ProdConfig(Config): 
-    pass 
+    SECRET_KEY = '\x17fT\x0e\x9f\xc3\xff1\x19\xcdT\x8b\xc0\x95\xf8\xf5\x8b\xc9\x8a\xa6N\xa5\x8d\x8e'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
  
 class DevConfig(Config): 
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SECRET_KEY = '\x17fT\x0e\x9f\xc3\xff1\x19\xcdT\x8b\xc0\x95\xf8\xf5\x8b\xc9\x8a\xa6N\xa5\x8d\x8e'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
